ad86757ba27373112020f12191b7b47a3d34c00f
jh0222 <gihyuny0222@gmail.com>
2023-12-29 15:23:49 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  //Logic
-  searchInputEl.focus();
-});
-
-// search 부분을 클릭했을 때
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// search 부분 이외를 클릭했을 때
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -150,6 +130,3 @@ styEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller); 
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2023
